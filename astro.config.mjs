60b20b6953e1c91282e2cb8946010d4a92fc262b
LaileF <lailefletcher@gmail.com>
2026-03-23 21:58:04 +0000

    enable server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://signalroutes.work",
-  output: "static",
-  integrations: [mdx(), sitemap()]
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [mdx(), sitemap()],
 });
