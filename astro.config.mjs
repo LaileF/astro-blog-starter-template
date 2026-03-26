40e3e90ea4d2ea48ef989b8c4bf4c7ac9ed50818
LaileF <lailefletcher@gmail.com>
2026-03-26 06:20:13 +0000

    allow hosts for railway + custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://signalroutes.work",
-  integrations: [mdx(), sitemap()],
+  server: {
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['signalroutes.work','.railway.app ']
+  }
 });
